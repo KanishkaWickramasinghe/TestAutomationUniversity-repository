12d7bcecd300da3505fab1a8fc2c15c92c241606
kwickramasingh <kwickramasingh@LKCONKWICKRAM02.infor.com>
2021-07-20 22:24:32 +0530

    Base test page modification

--- TestAutomationUniversity/src/test/java/testCases/BaseTest.java
+++ TestAutomationUniversity/src/test/java/testCases/BaseTest.java
@@ -1,8 +1,16 @@
 package testCases;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
+
+import com.google.common.io.Files;
 
 import pages.HomePage;
 import utils.WindowManager;
@@ -33,6 +41,24 @@ public class BaseTest {
 	{
 		driver.close();
 	}
+	
+	@AfterMethod
+	public void getTCFailure(ITestResult results) 
+	{
+//		if (ITestResult.FAILURE==results.getStatus()) 
+//		{
+			var camera= (TakesScreenshot)driver;
+			File screenshots=camera.getScreenshotAs(OutputType.FILE);
+			System.out.println("Screenshot Taken: "+screenshots.getAbsolutePath());
+			try {
+				Files.move(screenshots, new File("src/main/resources/test_"+results.getStatus()+".png"));
+				System.out.println(new File("src/main/resources/test_"+results.getStatus()+".png").getAbsolutePath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		//}
+	}
+	
 	
 	public WindowManager provideHadleToWindowManager()
 	{
